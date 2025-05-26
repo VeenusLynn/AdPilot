39ee7cd17a02cb255e4cb3974776e2f1b93f8d1a
Lyna NOURA <l.noura@esi-sba.dz>
2025-05-27 00:07:26 +0800

    small fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
           <CssBaseline />
           <NotificationProvider>
             <Routes>
-              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/" element={<Navigate to="/landing" replace />} />
               <Route path="/landing" element={<Landing />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
